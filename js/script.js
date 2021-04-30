0aa8e2e99f4a97161f95193805c75f2ce84bc1a2
tiago-1991 <53984116+tiago-1991@users.noreply.github.com>
2021-04-30 19:24:27 -0300

    Primeira inclusão
    Arquivos adaptados

---
+++ js/script.js
@@ -0,0 +1,112 @@
+
+(function () {
+  'use strict'
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+function mascara(objeto,funcao){
+    v_obj=objeto
+    v_fun=funcao
+    setTimeout("execmascara()",1);
+}
+function execmascara(){
+  v_obj.value=v_fun(v_obj.value);
+}
+
+function id(objeto){
+  return document.getElementById(objeto);
+}
+
+function mcpf(v){ //Mascara para CPF
+  v=v.replace(/\D/g,"")                        //Remove tudo o que não é dígito
+  v=v.replace(/(\d{3})(\d)/,"$1.$2")          //Coloca um ponto entre o terceiro e o quarto dígitos
+  v=v.replace(/(\d{3})(\d)/,"$1.$2")          //Coloca um ponto entre o terceiro e o quarto dígitos no segundo bloco
+  v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2")    //Coloca um hífen entre o terceiro e quarto dígitos
+  return v;
+}
+
+function mcep(v){  //MASCARA PARA CEP
+    v=v.replace(/\D/g,"")                      //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{5})(\d)/,"$1-$2")         //Esse é tão fácil que não merece explicações
+    return v;
+}
+
+function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+           // document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+            //document.getElementById('ibge').value=("");
+    }
+
+function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+           // document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+           // document.getElementById('ibge').value=(conteudo.ibge);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+               // document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                //document.getElementById('ibge').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
